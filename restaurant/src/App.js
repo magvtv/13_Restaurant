b95488c5082f4f69ae88799b512aa210f9798592
magvtv <magutujr@gmail.com>
2023-06-05 20:59:27 +0300

    added app and index to custom src

---
+++ restaurant/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <h1>
+            Nothing here, just chilling
+        </h1>
+    </div>
+  )
+}
+
+export default App;
